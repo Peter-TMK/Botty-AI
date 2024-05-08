feaa2d2941518a43151fd24318318cf5d354ba88
Peter-TMK <tundepeter01@gmail.com>
2024-05-08 19:27:27 +0100

    signup implementation

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 
-const app = express();
+import authRouter from "./routes/auth.routes.js";
+import connectDB from "./confg/db.js";
 dotenv.config();
+
+const app = express();
 const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send(`Hey Botty! Running on http://localhost:${PORT}`);
-});
+app.use(express.json());
+
+app.use("/api/auth", authRouter);
+
+// app.get("/", (req, res) => {
+//   res.send(`Hey Botty! Running on http://localhost:${PORT}`);
+// });
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server running on http://localhost:${PORT}`);
 });
