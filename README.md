3135242712c4662abeada81ec2f015c6d17a385a
Peter-TMK <tundepeter01@gmail.com>
2024-05-09 15:11:32 +0100

    feat:implement 'send message' in chat

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Hey Botty!
+
+Inside the bash terminal, insert and run the code below to generate a random secret key:
+openssl rand -base64 32
---
+++ backend/models/message.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Message", MessageSchema);
