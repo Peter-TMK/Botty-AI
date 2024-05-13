19af6d40134abb035b29240f12038d0a0d6cdf4f
Peter-TMK <tundepeter01@gmail.com>
2024-05-13 09:14:32 +0100

    feat: user and message services implemented

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { getMessages, sendMessage } from "../controllers/message.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
+import { getUsersForSidebar } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get("/:id", protectRoute, getMessages);
-router.post("/send/:id", protectRoute, sendMessage);
+router.get("/", protectRoute, getUsersForSidebar);
+// router.post("/send/:id", protectRoute, sendMessage);
 // router.post("/login", login);
 // router.post("/logout", logout);
 
